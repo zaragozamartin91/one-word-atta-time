931a048ac6d216f50355a8e0a19d8a5aef47d53a
Martin Zaragoza <zaragozamartin91@gmail.com>
2021-11-25 22:03:16 +0100

    Improved upon DocumentPage logic

--- docs/javascripts/DocumentPage.js
+++ docs/javascripts/DocumentPage.js
@@ -26,50 +26,24 @@ export default class DocumentPage {
     }
 
     get words() {
-        return this.phrases.flatMap(a => a)
+        return this.phrases
+            .flatMap(p => p.split(/ +/))    // split phrases by spaces
+            .map(w => w.trim())             // trim words
     }
 
+    /**
+     * @returns {Array<string>} This page's phrases
+     */
     get phrases() {
-        const words = this.lines.flatMap(ln => ln.split(' '))
-        const phrases = []
-
-        const lastWordIndex = words.length - 1
-
-        for (let phraseStart = 0, phraseEnd = 0; phraseStart < words.length; phraseEnd = phraseEnd + 1) {
-            const currWord = words[phraseEnd]?.trim() || ''
-
-            const pageDone = phraseEnd >= lastWordIndex // last word reached
-            const ignoreWord = this.ellipsisMatch(currWord) || currWord.length == 0
-            const acknowledgeWord = !ignoreWord
-            const endOfPhraseFound = currWord.endsWith('.')
-
-            if (pageDone || acknowledgeWord && endOfPhraseFound) {
-                // page done or full phrase detected , push phrase into array
-                const phrase = words.slice(phraseStart, phraseEnd)
-                phrase.push(currWord)
-                phrases.push(this.curatePhrase(phrase))
-                phraseStart = phraseEnd + 1
-            }
-        }
-
-        return phrases
-    }
-
-    /**
-     * Checks if a word contains an ellipsis
-     * @param {String} w Word to check for ellipsis
-     * @returns True if this word contains an ellipsis
-     */
-    ellipsisMatch(w) {
-        return (w?.match(/\./g) || []).length > 1
-    }
-
-    /**
-     * Trims phrase and removes empty words from it
-     * @param {Array<String>} p Phrase to curate
-     * @returns Curated phrase
-     */
-    curatePhrase(p) {
-        return p.map(s => s.trim()).filter(s => s.length > 0)
+        const pageText = this.lines.reduce((accum, curr) => accum.concat(' ', curr))
+        const eliRex = /\.{2,}/
+        const eliStub = '__3ll1p515__'
+        const curatedText = pageText.replace(eliRex, eliStub)
+        return curatedText.split(/(\. | \.)/)               // dot&space = sentence end
+            .map(p => p.trim())                             // trim phrase
+            .filter(p => p.length > 0)                      // remove empty phrases
+            .filter(p => p !== '.')                         // remove 'JUST DOTS' phrases
+            .map(p => p.replace(eliStub, '...'))            // restore ellipsis for proper content
+            .map(p => p.endsWith('.') ? p : p.concat('.'))  // end prhases with dots
     }
 }
--- test/DocumentPage.test.js
+++ test/DocumentPage.test.js
@@ -5,14 +5,20 @@ test('lines getter should split phrases by dot', () => {
     const page = 1
     const lines = ['this is one line.', 'this is another line. This is within the same line.']
     const documentPage = DocumentPage.fromObject({ page, lines })
-    expect(documentPage.phrases.length).toBe(3) // lines have 3 dots in total hence 3 phrases
+    expect(documentPage.phrases).toStrictEqual([
+        'this is one line.',
+        'this is another line.',
+        'This is within the same line.'
+    ])
 })
 
 test('lines getter should not treat ellipsis as a phrase end', () => {
-    const lines = ['this is one line...', 'this is not another phrase. but this is.']
+    const lines = ['First phrase... ', 'continuation of first phrase. Second phrase']
     const documentPage = new DocumentPage(1, lines)
-    expect(documentPage.phrases.length).toBe(2)
-})
+    expect(documentPage.phrases).toStrictEqual([
+        'First phrase... continuation of first phrase.',
+        'Second phrase.'
+    ])})
 
 test('words getter should parse phrases and flat map its contents', () => {
     const page = 1
